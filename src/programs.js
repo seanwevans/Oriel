c543de2864a3cf8e7a17d7d16a669d209ca22483
Sean Evans <sean.w.evans@gmail.com>
2025-12-06 12:12:31 -0500

    Extract icons and programs into modules

---
+++ src/programs.js
@@ -0,0 +1,314 @@
+export const PROGRAMS = [
+  {
+    type: "notepad",
+    title: "Notepad",
+    width: 300,
+    height: 200,
+    icon: "notepad",
+    label: "Notepad"
+  },
+  {
+    type: "write",
+    title: "Write",
+    width: 500,
+    height: 400,
+    icon: "write",
+    label: "Write"
+  },
+  {
+    type: "winfile",
+    title: "File Manager",
+    width: 500,
+    height: 350,
+    icon: "winfile",
+    label: "File Mgr"
+  },
+  {
+    type: "cardfile",
+    title: "Cardfile",
+    width: 350,
+    height: 400,
+    icon: "cardfile",
+    label: "Cardfile"
+  },
+  {
+    type: "calc",
+    title: "Calculator",
+    width: 220,
+    height: 250,
+    icon: "calc",
+    label: "Calculator"
+  },
+  {
+    type: "mines",
+    title: "Minesweeper",
+    width: 240,
+    height: 320,
+    icon: "mines",
+    label: "Minesweeper"
+  },
+  {
+    type: "solitaire",
+    title: "Solitaire",
+    width: 600,
+    height: 450,
+    icon: "solitaire",
+    label: "Solitaire"
+  },
+  {
+    type: "chess",
+    title: "Chess",
+    width: 640,
+    height: 520,
+    icon: "chess",
+    label: "Chess"
+  },
+  {
+    type: "reversi",
+    title: "Reversi",
+    width: 300,
+    height: 340,
+    icon: "reversi",
+    label: "Reversi"
+  },
+  {
+    type: "paint",
+    title: "Paintbrush",
+    width: 500,
+    height: 400,
+    icon: "paint",
+    label: "Paintbrush"
+  },
+  {
+    type: "photoshop",
+    title: "Photoshop 1.0",
+    width: 760,
+    height: 560,
+    icon: "photoshop",
+    label: "Photoshop"
+  },
+  {
+    type: "mplayer",
+    title: "Media Player",
+    width: 350,
+    height: 250,
+    icon: "mplayer",
+    label: "Media Player"
+  },
+  {
+    type: "simcity",
+    title: "Micropolis",
+    width: 560,
+    height: 520,
+    icon: "simcity",
+    label: "Micropolis"
+  },
+  {
+    type: "skifree",
+    title: "SkiFree",
+    width: 520,
+    height: 520,
+    icon: "skifree",
+    label: "SkiFree"
+  },
+  {
+    type: "linerider",
+    title: "Line Rider",
+    width: 620,
+    height: 520,
+    icon: "linerider",
+    label: "Line Rider"
+  },
+  {
+    type: "soundrec",
+    title: "Sound Recorder",
+    width: 300,
+    height: 160,
+    icon: "soundrec",
+    label: "Sound Rec"
+  },
+  {
+    type: "radio",
+    title: "Radio",
+    width: 620,
+    height: 460,
+    icon: "radio",
+    label: "Radio"
+  },
+  {
+    type: "beatmaker",
+    title: "Beat Lab",
+    width: 720,
+    height: 420,
+    icon: "beatmaker",
+    label: "Beat Lab"
+  },
+  {
+    type: "clock",
+    title: "Clock",
+    width: 250,
+    height: 250,
+    icon: "clock",
+    label: "Clock"
+  },
+  {
+    type: "charmap",
+    title: "Character Map",
+    width: 460,
+    height: 380,
+    icon: "charmap",
+    label: "Char Map"
+  },
+  {
+    type: "control",
+    title: "Control Panel",
+    width: 400,
+    height: 300,
+    icon: "control",
+    label: "Control"
+  },
+  {
+    type: "imageviewer",
+    title: "Image Viewer",
+    width: 720,
+    height: 540,
+    icon: "imageviewer",
+    label: "Image View"
+  },
+  {
+    type: "pdfreader",
+    title: "PDF Reader",
+    width: 720,
+    height: 540,
+    icon: "pdfreader",
+    label: "PDF Reader"
+  },
+  {
+    type: "markdown",
+    title: "Markdown Viewer",
+    width: 700,
+    height: 500,
+    icon: "markdown",
+    label: "Markdown"
+  },
+  {
+    type: "clipbrd",
+    title: "Clipboard",
+    width: 300,
+    height: 250,
+    icon: "clipboard",
+    label: "Clipboard"
+  },
+  {
+    type: "taskman",
+    title: "Task List",
+    width: 320,
+    height: 350,
+    icon: "taskman",
+    label: "Task List"
+  },
+  {
+    type: "database",
+    title: "Data Manager",
+    width: 500,
+    height: 400,
+    icon: "database",
+    label: "Data Mgr"
+  },
+  {
+    type: "compiler",
+    title: "Tiny C",
+    width: 450,
+    height: 350,
+    icon: "ccompiler",
+    label: "Tiny C"
+  },
+  {
+    type: "python",
+    title: "Tiny Python",
+    width: 450,
+    height: 350,
+    icon: "python",
+    label: "Python"
+  },
+  {
+    type: "console",
+    title: "Console",
+    width: 500,
+    height: 350,
+    icon: "console",
+    label: "Console"
+  },
+  {
+    type: "hexedit",
+    title: "Hex Editor",
+    width: 720,
+    height: 520,
+    icon: "hexedit",
+    label: "Hex Editor"
+  },
+  {
+    type: "rss",
+    title: "RSS Reader",
+    width: 720,
+    height: 520,
+    icon: "rss",
+    label: "RSS Reader"
+  },
+  {
+    type: "browser",
+    title: "Web Browser",
+    width: 640,
+    height: 480,
+    icon: "browser",
+    label: "Browser"
+  },
+  {
+    type: "radiogarden",
+    title: "Radio Garden",
+    width: 720,
+    height: 520,
+    icon: "radiogarden",
+    label: "Radio Garden"
+  },
+  {
+    type: "discord",
+    title: "Discord (API)",
+    width: 720,
+    height: 520,
+    icon: "discord",
+    label: "Discord"
+  },
+  {
+    type: "irc",
+    title: "IRC Client",
+    width: 680,
+    height: 500,
+    icon: "irc",
+    label: "IRC"
+  },
+  {
+    type: "readme",
+    title: "Read Me",
+    width: 350,
+    height: 400,
+    icon: "readme",
+    label: "Read Me"
+  },
+  {
+    type: "doom",
+    title: "DOOM",
+    width: 640,
+    height: 400,
+    icon: "doom",
+    label: "DOOM"
+  },
+  {
+    type: "papers",
+    title: "Checkpoint",
+    width: 640,
+    height: 520,
+    icon: "papers",
+    label: "Checkpoint"
+  }
+];
